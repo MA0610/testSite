e958b06cba3cfc39763aaba7b513a5c2704091dc
MA0610 <47308923+MA0610@users.noreply.github.com>
2024-10-09 19:26:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,9 @@ def schedule():
     day = data.get('day')
     time_slot_index = int(data.get('time_slot'))
     class_name = data.get('class')
+    slot = data.get('dataDay') #USE THIS TO PUT DATA IN CORRECT SLOT in visualization
+    print(slot)
+
     
     if day in day_to_index and 0 <= time_slot_index < NUM_TIME_SLOTS:
         day_index = day_to_index[day]
@@ -85,68 +88,16 @@ def remove_class():
 def view_schedules():
     return jsonify(schedules_3d)
 
+@app.route('/display_schedules', methods=['GET'])
+def display_schedules():
+    time_slots = ["8:30-9:25 MW", "8:00-9:25 am MF", "8:30-9:25 am M-F", "8:30-9:25 am MWF", "9:35-10:30 am MWF","10:40-11:35 am MWF","11:45-12:40 pm MWF","1:55-2:50 pm M-F","1:55-2:50 pm MWF","1:55-2:50 pm MWF","3:00-3:55 pm MWF","3:00-5:55 pm M-LAB","4:05-5:00 pm MWF","4:05-5:30 pm MW","4:05-5:30 pm MF","5:40-7:05 pm MW","7:15-8:40 pm MW","7:15-10:10 pm M-LAB","8:50-10:15 pm MW"]
+    
+    return render_template('schedules.html', schedules=schedules_3d, time_slots=time_slots)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-#M-F class sets (0)
-#8:30-9:25 am
-#1:55-2:50 pm
 
-#MWF class sets (1)
-#8:30-9:25 am
-#9:35-10:30 am
-#10:40-11:35 am
-#11:45-12:40 pm
-#1:55-2:50 pm
-#3:00-3:55 pm
-#4:05-5:00 pm
-
-#TR class sets (2)
-#8:30-9:55 am
-#10:05-11:30 am
-#1:30-2:55 pm
-#3:05-4:30 pm
-#5:00-6:25 pm
-
-#MW class sets (3)
-#8:00-9:25 am
-#4:05-5:30 pm
-#5:40-7:05 pm
-#7:15-8:40 pm
-#8:50-10:15 pm
-
-#MF class sets (4)
-#8:00-9:25 am
-#4:05-5:30 pm
-
-#MW class sets (5)
-#8:00-9:25 am
-#4:05-5:30 pm
-
-#T OR R class sets (6)
-#1:30-4:25 pm
-#8:30-11:25 am
-#1:30-4:25 pm
-
-
-#T OR W OR R class sets (7)
-#7:15-10:15 pm
-
-
-###############
-#LABS
-
-#T OR R class sets (8)
-#8:30-11:25 am
-#1:30-4:25 pm
-
-#W OR F (9)
-#1:55-4:50 pm
-
-#M OR W (10)
-#3:00-5:55 pm
-
-#M OR W OR R (11)
-#7:15-10:10 pm
+    
